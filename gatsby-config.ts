a068a4e561124e84c91ba9fc545c7a984afb584e
Faith <faiththe1rst@gmail.com>
2022-05-21 20:04:14 +0100

    Initial commit from gatsby: (https://github.com/jpedroschmitz/gatsby-starter-ts.git)

---
+++ gatsby-config.ts
@@ -0,0 +1,9 @@
+import type { GatsbyConfig } from 'gatsby';
+
+const config: GatsbyConfig = {
+  // Since `gatsby-plugin-typescript` is automatically included in Gatsby you
+  // don't need to define it here (just if you need to change the options)
+  plugins: [],
+};
+
+export default config;
